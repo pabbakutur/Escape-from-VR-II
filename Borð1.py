e6531c0026ba58a5571c315f318c29bcc939f1bf
Ari Sigþór Eiríksson <ari.sigthor@gmail.com>
2019-03-19 09:32:00 +0000

    versionVilluprofun1

--- Borð1.py
+++ Borð1.py
@@ -9,6 +9,9 @@ def main():
         {"spurning": "Svar 2?",
          "svar": [ "1" , "2" , "3" , "4"],
          "rétt": "2"} ,
+         {"spurning": "Svar 2?",
+         "svar": [ "1" , "2" , "3" , "4"],
+         "rétt": "2"} ,
          ]
     print("\n")
     print("Spurningaleikur! \n")
@@ -17,14 +20,43 @@ def main():
     print("Menu\n"
           "1. Byrja leik\n"
           "2. Loka leik\n")
-    val = int(input("Veldu möguleika: "))
+    command=velja()
     print("")
-    while int(val) not in range(1, 3):
-        val = input("Veldu 1 eða 2: ")
-    if val == 1:
+    if command == "1":
         spila(spurningar)
-    elif val == 2:
+    elif command == "2":
         exit
+    else:
+        print("þetta er ekki valmöguleiki, veldu 1 eða 2 !")
+        veljaAftur()
+
+def veljaAftur():
+    spurningar = [
+        {"spurning": "Svar 3?",
+         "svar": [ "1" , "2" , "3" , "4"],
+         "rétt": "3"} ,
+        {"spurning": "Hvernig er banani á litinn?",
+        "svar":["gulur" , "rauður" , "grænn" , "blár"],
+        "rétt": "1"} ,
+        {"spurning": "Svar 2?",
+         "svar": [ "1" , "2" , "3" , "4"],
+         "rétt": "2"} ,
+         {"spurning": "Svar 2?",
+         "svar": [ "1" , "2" , "3" , "4"],
+         "rétt": "2"} ,
+         ]
+    command = velja()
+    if command == "1":
+        spila(spurningar)
+    elif command == "2":
+        exit
+    else:
+        print("þetta er ekki valmöguleiki, veldu 1 eða 2!")
+        veljaAftur()
+
+def velja():
+    x = input("Veldu svar: ")
+    return x
 
 import random
 def spila(spurningar):
@@ -37,17 +69,26 @@ def spila(spurningar):
         print(spurning["spurning"])
         for i, val in enumerate(spurning["svar"]):
             print(str(i + 1) + ". " + val)
-        answer = input("Veldu svar: ")
-        while int(answer)-1 not in range(len(spurning["svar"])):
-            print("\nÞetta er ekki svarmöguleiki, reyndu aftur. \n")
-            answer = input("\nVeldu svar: ")
+        answer=velja()
+
         if answer == spurning["rétt"]:
             score += 1
             total += 1
             print("\nÞað er rétt.\n")
-        else:
+        elif answer == "1" :
             print("\nÞað er rangt. \n")
             total +=1
+        elif answer == "2":
+            print("\nÞað er rangt. \n")
+            total +=1
+        elif answer =="3":
+            print("\nÞað er rangt. \n")
+            total +=1
+        elif answer == "4":
+            print("\nÞað er rangt. \n")
+            total +=1
+        else:
+            print("þetta er ekki svarmöguleiki, reyndu aftur")
         print("Stigastaða: ", score, "af", total, "\n")
         if score - total == -2:
             print("Þú tapaðir")
