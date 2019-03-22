48b3c136e85f6966da609ba1c1c55e825a69128d
Arnar Guðnason <arg66@hi.is>
2019-03-22 15:01:53 +0000

    q

--- prenta.py
+++ prenta.py
@@ -1,8 +1,29 @@
 import sys
 import time
 
-string = 'hello world\n'
+string = "Lexi situr á lesstofunni að leggja lokahönd á heimavinnuna sína.\nHann hefur aldrei verið sterkur að heilda eða diffra, þú þarft að hjálpa honum.\nÞú færð 3 tilraunir til að svara 2 spurningum réttum.\n"
 for char in string:
     sys.stdout.write(char)
     sys.stdout.flush()
-    time.sleep(.25)
+    time.sleep(.15)
+
+
+##################
+
+string = "Lexi situr á lesstofunni að leggja lokahönd á heimavinnuna sína.\n"
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.15)
+
+string = "Hann hefur aldrei verið sterkur að heilda eða diffra, þú þarft að hjálpa honum.\n"
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.15)
+
+string = "Þú færð 3 tilraunir til að svara 2 spurningum réttum.\n"
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.15)
