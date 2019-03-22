e820cc0f372a2b7a0a043c2a3a05aca1b42ad40b
Arnar Guðnason <arg66@hi.is>
2019-03-22 09:39:37 +0000

    unittest

--- test_Leikur.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from Leikur import kynning
-
-class testLeikur(unittest.TestCase):
-    def fname(arg):
-        pass
---
+++ test_bord1.py
@@ -0,0 +1,6 @@
+import unittest
+from bord1 import spila
+
+class testBord1(unittest.TestCase):
+    def fname(arg):
+        pass
