14b1ad14eee0df627e9525dd19d8c236c1731d80
Ari Sigþór Eiríksson <ari.sigthor@gmail.com>
2019-03-24 14:32:50 +0000

    versionsexy

--- Borð1.py
+++ Borð1.py
@@ -1,102 +1,92 @@
-def main():
-    spurningar = [
-        {"spurning": "Svar 3?",
-         "svar": [ "1" , "2" , "3" , "4"],
-         "rétt": "3"} ,
-        {"spurning": "Hvernig er banani á litinn?",
-        "svar":["gulur" , "rauður" , "grænn" , "blár"],
-        "rétt": "1"} ,
-        {"spurning": "Svar 2?",
-         "svar": [ "1" , "2" , "3" , "4"],
-         "rétt": "2"} ,
-         {"spurning": "Svar 2?",
-         "svar": [ "1" , "2" , "3" , "4"],
-         "rétt": "2"} ,
-         ]
-    print("\n")
-    print("Spurningaleikur! \n")
-    print("Svaraðu 2 spurningum rétt til að vinna\n")
-    print("Þú hefur 3 tilraunir\n")
-    print("Menu\n"
-          "1. Byrja leik\n"
-          "2. Loka leik\n")
-    command=velja()
-    print("")
-    if command == "1":
-        spila(spurningar)
-    elif command == "2":
-        exit
-    else:
-        print("þetta er ekki valmöguleiki, veldu 1 eða 2 !")
-        veljaAftur()
-
-def veljaAftur():
-    spurningar = [
-        {"spurning": "Svar 3?",
-         "svar": [ "1" , "2" , "3" , "4"],
-         "rétt": "3"} ,
-        {"spurning": "Hvernig er banani á litinn?",
-        "svar":["gulur" , "rauður" , "grænn" , "blár"],
-        "rétt": "1"} ,
-        {"spurning": "Svar 2?",
-         "svar": [ "1" , "2" , "3" , "4"],
-         "rétt": "2"} ,
-         {"spurning": "Svar 2?",
-         "svar": [ "1" , "2" , "3" , "4"],
-         "rétt": "2"} ,
-         ]
-    command = velja()
-    if command == "1":
-        spila(spurningar)
-    elif command == "2":
-        exit
-    else:
-        print("þetta er ekki valmöguleiki, veldu 1 eða 2!")
-        veljaAftur()
-
-def velja():
-    x = input("Veldu svar: ")
-    return x
-
+import sys
+from time import sleep
+import time
 import random
-def spila(spurningar):
-    print("\n")
-    score = 0
-    total = 0
-    random.shuffle(spurningar)
-    for spurning in spurningar:
-        print("Veldu 1, 2, 3 eða 4")
-        print(spurning["spurning"])
-        for i, val in enumerate(spurning["svar"]):
-            print(str(i + 1) + ". " + val)
-        answer=velja()
 
-        if answer == spurning["rétt"]:
-            score += 1
-            total += 1
-            print("\nÞað er rétt.\n")
-        elif answer == "1" :
-            print("\nÞað er rangt. \n")
-            total +=1
-        elif answer == "2":
-            print("\nÞað er rangt. \n")
-            total +=1
-        elif answer =="3":
-            print("\nÞað er rangt. \n")
-            total +=1
-        elif answer == "4":
-            print("\nÞað er rangt. \n")
-            total +=1
-        else:
-            print("þetta er ekki svarmöguleiki, reyndu aftur")
-        print("Stigastaða: ", score, "af", total, "\n")
-        if score - total == -2:
-            print("Þú tapaðir")
-            break
-        elif score == 2:
-            print("Þú vannst")
-            break
-    print("Þú varst með", score, "rétt af", total ,"\n")
-    exit
+class Bord1():
+    def __init__(self):
+        self.score= 0
+        self.total= 0
+        self.spurningar = [
+    {"spurning": "4x^3cos(x^4) dx = ?",
+    "svar": [ "1/4cos(x^4)" , "cos(x^4) + C" , "sin(x^4) + C" , "16x^4sin(x^4) + C"],
+    "rétt": "3"} ,
+    {"spurning": "d/dx(1/2Sin(x^2) = ?)",
+    "svar":["cos(x)" , "cos(x^2)" , "xcos(x^2)" , "xcos(x)"],
+    "rétt": "1"} ,
+    {"spurning": "d/dx tan(x) = ?",
+     "svar": [ "sec(x)" , "2sec(x^2)" , "1/2sec(x)" , "sec^2(x)"],
+     "rétt": "4"},]
 
-main()
+    def velja(self):
+        x=input("Veldu svar: ")
+        return x
+
+    def byrja(self):
+        print('') ; sleep(1)
+        print('') ; sleep(1)
+        print('') ; sleep(1)
+        print('$$$$$$$$$$$$$$$$$$$$$') ; sleep(1)
+        print('$$   HEIMAVINNAN   $$') ; sleep(1)
+        print('$$$$$$$$$$$$$$$$$$$$$') ; sleep(1)
+        print('') ; sleep(1)
+        print('') ; sleep(1)
+        print('') ; sleep(1)
+        string = "Lexi situr á lesstofunni að leggja lokahönd á heimavinnuna sína.\nHann hefur aldrei verið sterkur að heilda eða diffra, þú þarft að hjálpa honum.\nÞú færð 3 tilraunir til að svara 2 spurningum réttum."
+        for char in string:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(.10)
+        print('') ; sleep(0.5)
+        print('') ; sleep(0.5)
+        while 1:
+            a = input('Ýttu á ENTER til að halda áfram')
+            if(len(a)<1):
+                self.spila(self.spurningar)
+
+    def spila(self, spurningar):
+        print("\n")
+        random.shuffle(spurningar)
+        for spurning in spurningar:
+            print("Veldu 1, 2, 3 eða 4")
+            print(spurning["spurning"])
+            for i, val in enumerate(spurning["svar"]):
+                print(str(i + 1) + ". " + val)
+            answer= self.velja()
+            if answer == spurning["rétt"]:
+                self.score = self.score + 1
+                self.total = self.total + 1
+                print("\nÞað er rétt.\n")
+            elif answer == "Break":
+                sys.exit()
+            elif answer == "1":
+                print("\nÞað er rangt. \n")
+                self.total +=1
+            elif answer == "2":
+                print("\nÞað er rangt. \n")
+                self.total +=1
+            elif answer == "3":
+                print("\nÞað er rangt. \n")
+                self.total +=1
+            elif answer == "4":
+                print("\nÞað er rangt. \n")
+                self.total +=1
+            else:
+                print("Þetta er ekki valmöguleiki, reyndu aftur")
+            print("Stigastaða: ", self.score, "af", self.total, "\n")
+            if self.score - self.total == -2:
+                print("Þú tapaðir og þarft að byrja aftur.")
+                B1.byrja()
+            elif self.score == 2:
+                print("Þú vannst")
+                sys.exit()
+                #láta fara á næsta borð
+            else:
+                self.spila(self.spurningar)
+            ###########
+
+        print("Þú varst með", self.score, "rétt af", self.total ,"\n")
+        exit
+
+B1=Bord1()
+B1.byrja()
