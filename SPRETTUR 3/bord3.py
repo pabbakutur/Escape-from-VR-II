39b954aa694eeb588a8c882c080754f4c0af7464
ari53 <ark25@hi.is>
2019-04-11 01:01:26 +0000

    Created base class. Used class in Bord3

--- SPRETTUR 3/bord3.py
+++ SPRETTUR 3/bord3.py
@@ -1,183 +1,98 @@
 import sys
 import time
 import random
-#from bord import Bord
-#from bord4 import Bord4
-#from bord2 import Bord2
-#class Bord3(Bord):
-class Bord3:
+from bord import Bord
+class Bord3(Bord):
     def __init__(self):
         self.kennarar = ['SIGURÐUR ÖRN','RÖGNVALDUR MÖLLER','INGÓLFUR HJÖRLEIFSSON','GUNNAR SCHRAM','VILMUNDUR TORFI','SIGURÐUR FREYR']
         self.fraegir = ['PYTHAGORAS','ARCHIMEDES','FROBENIUS','EULER','FIBONACCI','ALBERT EINSTEIN']
         self.hugtok = ['JACOBI FYLKI','MILLIGILDISSETNINGIN','KLEMMUREGLAN','HESSE FYLKI','LEIFASETNINGIN','CAUCHY SETNINGIN']
-
-    #Hreinsum skjáinn (30 auðar línur)
-    def clear(self):
-        for i in range(30):
-           print ('\n')
-
-        #Þetta fall prentar út mynd af hengimanni á skipanalínu. Index er fjöldi skrefa eftir
-        #og ætti að vera >=0
-    def hengimadurprent(self,index):
-            if index==0:
-                    print('         _____ ')
-                    print('         |   | ')
-                    print('         O   | ')
-                    print('        /|\\  | ')
-                    print('        / \\  | ')
-                    print('             | ')
-                    print('     ________|_')
-                    return
-            if index==1:
-                    print('         _____ ')
-                    print('         |   | ')
-                    print('         O   | ')
-                    print('        /|\\  | ')
-                    print('        /    | ')
-                    print('             | ')
-                    print('     ________|_')
-                    return
-            if index==2:
-                    print('         _____ ')
-                    print('         |   | ')
-                    print('         O   | ')
-                    print('        /|\\  | ')
-                    print('             | ')
-                    print('             | ')
-                    print('     ________|_')
-                    return
-            if index==3:
-                    print('         _____ ')
-                    print('         |   | ')
-                    print('         O   | ')
-                    print('        /|   | ')
-                    print('             | ')
-                    print('             | ')
-                    print('     ________|_')
-                    return
-            if index==4:
-                    print('         _____ ')
-                    print('         |   | ')
-                    print('         O   | ')
-                    print('         |   | ')
-                    print('             | ')
-                    print('             | ')
-                    print('     ________|_')
-                    return
-            if index==5:
-                    print('         _____ ')
-                    print('         |   | ')
-                    print('         O   | ')
-                    print('             | ')
-                    print('             | ')
-                    print('             | ')
-                    print('     ________|_')
-                    return
-            if index==6:
-                    print('         _____ ')
-                    print('         |   | ')
-                    print('             | ')
-                    print('             | ')
-                    print('             | ')
-                    print('             | ')
-                    print('     ________|_')
-                    return
-            if index==7:
-                    print('         _____ ')
-                    print('             | ')
-                    print('             | ')
-                    print('             | ')
-                    print('             | ')
-                    print('             | ')
-                    print('     ________|_')
-                    return
-            if index==8:
-                    print('               ')
-                    print('             | ')
-                    print('             | ')
-                    print('             | ')
-                    print('             | ')
-                    print('             | ')
-                    print('     ________|_')
-                    return
-            if index==9:
-                    print('               ')
-                    print('               ')
-                    print('               ')
-                    print('               ')
-                    print('               ')
-                    print('               ')
-                    print('     ________|_')
-                    return
+        self.aHengimadur =[
+            '         _____ \n         |   | \n         O   | \n        /|\\  | \n        / \\  | \n             | \n     ________|_', #0
+            '         _____ \n         |   | \n         O   | \n        /|\\  | \n        /    | \n             | \n     ________|_',  #1
+            '         _____ \n         |   | \n         O   | \n        /|\\  | \n             | \n             | \n     ________|_',  #2
+            '         _____ \n         |   | \n         O   | \n        /|   | \n             | \n             | \n     ________|_',   #3
+            '         _____ \n         |   | \n         O   | \n         |   | \n             | \n             | \n     ________|_',   #4
+            '         _____ \n         |   | \n         O   | \n             | \n             | \n             | \n     ________|_',   #5
+            '         _____ \n         |   | \n             | \n             | \n             | \n             | \n     ________|_',   #6
+            '         _____ \n             | \n             | \n             | \n             | \n             | \n     ________|_',   #7
+            '               \n             | \n             | \n             | \n             | \n             | \n     ________|_',   #8
+            '               \n               \n               \n               \n               \n               \n     ________|_'  ] #9
+        # Þarf að initialize-a base klasann                
+        super(Bord3,self).__init__()
 
         #Borð 3 hefst hér, spilari getur valið um 3 möguleika. Byrja, upplýsingar eða hætta.
     def startgluggi(self):
-        self.clear()
-        print('######################################')
-        print('#         Velkominn í borð 3         #')
-        print('#      Hengimaður að hætti Lexa      #')
-        print('######################################')
-        print('#       1.Byrja                      #')
-        print('#       2.Upplýsingar um borð 3      #')
-        print('#       3.Hætta                      #')
-        print('######################################')
-        choice = input('Veldu möguleika 1,2 eða 3: ')
-        return choice
+        self.clearScreen()
+        self.setHeader(
+            'Velkominn í borð 3\n'+
+            'Hengimaður að hætti Lexa')
+        self.setDirection('\n')
+        self.setOptions(
+            '1.Byrja\n'+
+            '2.Upplýsingar um borð 3\n'+
+            '3.Hætta')
+        self.setStats('\n')
+        self.printGameScreen()
+        return self.GetKeypressS('123','Veldu möguleika')
         #return self.GetKeypressS('123')
         
         #Spilari fær að velja mismunandi orðaflokka
     def startgluggi2(self):
-        self.clear()
-        print('######################################')
-        print('#   Nú hefur þú kost á að velja 3    #')
-        print('#   mismunandi orðaflokka sem eru    #')
-        print('#   í miklu uppáhaldi hjá Lexa.      #')
-        print('######################################')
-        print('        1.Kennarar í HÍ              #')
-        print('        2.Frægir stærðfræðingar      #')
-        print('        3.Stærðfræðihugtök           #')
-        print('######################################')
-        choice = input('Veldu möguleika 1,2 eða 3: ')
-        return choice
+        self.clearScreen()
+        self.setHeader(
+            'Nú hefur þú kost á að velja 3\n'+
+            'mismunandi orðaflokka sem eru\n'+
+            'í miklu uppáhaldi hjá Lexa.')
+        
+        self.setOptions(
+            '1.Kennarar í HÍ\n'+
+            '2.Frægir stærðfræðingar\n'+
+            '3.Stærðfræðihugtök')
+        self.printGameScreen()
+        ret = self.GetKeypressS('123','Veldu möguleika')
+        if ret == '1':
+            self.setHeader('Reyndu að finna nafn á kennara í HÍ')
+        elif ret == '2':
+            self.setHeader('Reyndu að finna nafn á frægum stærðfræðingi')
+        else:
+            self.setHeader('Reyndu að giska á frægt stærðfræðihugtak')
+        return ret
         #return self.GetKeypressS('123')
 
         #Fall sem inniheldur upplýsingar um borð 3.  Til að fara úr því þarf að ýta á einhvern takka
     def upplysingar(self):
-        self.clear()
-        print('#######################################################################')
-        print('#     Það sem fáir vita um Lexa er að uppáhalds leikur Lexa er        #')
-        print('#     hinn klassíski hengimaður. Markmið leiksins er að spilari       #')
-        print('#     reynir að komast að því hvaða orð tölvan hefur valið með því    #')
-        print('#     að giska á einn staf í einu. Ef spilari giskar á vitlausan staf #')
-        print('#     þá missir hann eina tilraun. Leikmaður hefur í heildina 9 til-  #')
-        print('#     raunir og ef hann klúðrar þeim öllum þá er maðurinn hengdur.    #')
-        print('#     Leikurinn er sérsniðinn að Lexa svo það eru einungis orð sem    #')
-        print('#     Lexi elskar. Njótið.                                            #')
-        print('#######################################################################')
-        print('Ýttu á einhvern staf á lyklaborðinu til að fara til baka')
-        input('')
-        return
+        self.clearScreen()
+        self.setDirection(
+            'Það sem fáir vita um Lexa er að uppáhalds leikur Lexa er\n'+
+            'hinn klassíski hengimaður. Markmið leiksins er að spilari\n'+
+            'reynir að komast að því hvaða orð tölvan hefur valið með því\n'+
+            'að giska á einn staf í einu. Ef spilari giskar á vitlausan staf\n'+
+            'þá missir hann eina tilraun. Leikmaður hefur í heildina 9 til-\n'+
+            'raunir og ef hann klúðrar þeim öllum þá er maðurinn hengdur.\n'+
+            'Leikurinn er sérsniðinn að Lexa svo það eru einungis orð sem\n'+
+            'Lexi elskar. Njótið.\n')
+        self.printGameScreen()
+        return self.GetKeypressS('','Ýttu á einhvern staf á lyklaborðinu til að fara til baka')
 
         #gameInterface er aðal user interface-ið í leiknum. Guess,miss_attempts á ekki að vera >9, misses er öll röngu giskin
         #prentar út interface og skilar # ef spilari vill endurræsa, ! ef spilari vill svarið.
     def gameInterface(self,guess,miss_attempts,misses,hintleft):
-        self.clear()
+        self.clearScreen()
         left = 9-miss_attempts
-        self.hengimadurprent(left)
-        print('###########################################')
-        print('Orð: ',end='')
-        for i in guess:
-                print(i,' ',end='')
-        print()
-        print('# Röng gisk: ',end='')
-        for i in misses:
-                print(i,' ',end='')
-        print()
+        self.setDirection(self.aHengimadur[left])
+        self.setOptions(
+            'Orð: ' + ' '.join(guess) +'\n'+
+            'Röng gisk: ' + ' '.join(misses))
+#        self.printGameScreen()
         if left != 0:
-            print('# Þú átt ',left,' tilraun(ir) eftir')
-            print('# Þú átt ',hintleft,' vísbendingu/ar eftir')
-            print('# Skrifaðu ? til að fá vísbendingu og ! til að hætta')
-            _in = input('# Giskaðu á staf: ')
+            self.setStats(
+                'Þú átt '+ str(left) + ' tilraun(ir) eftir\n'+
+                'Þú átt ' + str(hintleft) +' vísbendingu/ar eftir\n'+
+                'Skrifaðu ? til að fá vísbendingu og ! til að hætta'   )
+            self.printGameScreen()
+
+            _in = self.GetKeypressS('','Giskaðu á staf: ')
             if len(_in)>1:
                 return '<'
             else:
@@ -188,9 +103,9 @@ class Bord3:
                 else:
                     return '<'
         else:
-            print('# Þú tapaðir, skrifaðu inn einhvern staf til að reyna aftur!')
-            time.sleep(2)
-            _in = input('')
+            self.setStats('Þú tapaðir. Ekki nógu gott!')
+            self.printGameScreen()
+            _in = self.GetKeypressS('','Ýttu á einhvern staf til að reyna aftur!')
             return -1
 
         #Game is the process of gaming. Skilar false ef leikur endar. hintMax á ekki að vera stærri en 3.
@@ -220,18 +135,15 @@ class Bord3:
                             guess[i] = operation
                     hintTimes = hintTimes+1
                 else:
-                    print('#Vísbendingarnar eru búnar!')
-                    print('# Augnablik!')
-                    time.sleep(2)
+                    self.setStats('Vísbendingarnar eru búnar!')
+                    self.printGameScreen()
                     continue
             elif operation == '<':
-                print('# Vinsamlega sláðu inn 1 bókstaf')
-                print('# Augnablik!')
-                time.sleep(2)
+                self.GetKeypressS('','Vinsamlega sláðu inn 1 bókstaf')
                 continue
             elif operation == '!':
                 #sys.exit()
-                #self.clear()
+                #self.clearScreen()
                 #self.hengimadurprent(9-miss)
                 #print('###########################################')
                 #print('# Svarið er: ',word)
@@ -252,14 +164,10 @@ class Bord3:
                         misses.append(operation)
 
             if not '_' in guess:
-                self.clear()
-                self.hengimadurprent(9-miss)
-                print('###########################################')
-                print('# ',end='')
-                for i in guess:
-                    print(i,' ',end='')
-                print()
-                print('# Vel gert!')
+                self.clearScreen()
+                self.setDirection(self.aHengimadur[9-miss])
+                self.setOptions('Orð: ' + ' '.join(guess)+'\n\n'+'Vel gert!')
+                self.printGameScreen()
                 time.sleep(2)
                 keyra = False
                 #b4.gangur()
@@ -291,9 +199,8 @@ class Bord3:
             return self.hugtok[index]
 
     def keyra(self):
-        self.clear()
+        self.clearScreen()
         while True:
-            gameProcess = True
             sel = self.startgluggi()
             if sel=='1':
                 while True:
@@ -306,9 +213,7 @@ class Bord3:
                         continue
                     else:
                         word = self.getWord(int(sel2))
-                        #while gameProcess:
                         return self.game(word,4-int(sel2))
-                        #sys.exit()
             elif sel=='2':
                 self.upplysingar()
                 continue
@@ -320,3 +225,7 @@ class Bord3:
 #if kalla á borð 4
 #bord4=Bord4()
 #bord4.gangur()
+
+if __name__ == '__main__':
+    b = Bord3()
+    b.keyra()
